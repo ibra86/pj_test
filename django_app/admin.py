60e3d425825cda94f68af858b3d048be407118d0
Anton Ibragimov <ibrahimov.ag@gmail.com>
2017-04-19 00:51:42 +0200

    added Book model to store the notes

--- django_app/admin.py
+++ django_app/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Book, BookContent
 from .forms import PostForm
 
 class PostAdmin(admin.ModelAdmin):
     form = PostForm
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Book)
+admin.site.register(BookContent)
--- django_app/models.py
+++ django_app/models.py
@@ -11,9 +11,33 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
     photo = models.ImageField(blank=True,null=True)
 
+    class Meta:
+        ordering = ['pk']
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=200)
+    post = models.ManyToManyField(
+        Post,
+        through='BookContent',
+        )
+
+    class Meta:
+        ordering = ['title']
+
+    def __unicode__(self):
+        return self.title
+
+class BookContent(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return 'Post "%s" of Book "%s"' %(self.post, self.book)
